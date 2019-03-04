5e36265660b927406bc1363c103a8d347b075962
Pavan <40331883+iamPavan17@users.noreply.github.com>
2019-03-04 22:11:59 +0530

    Version 1.0.0

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+var API_KEY = '';   // your api key
+var DOMAIN = '';    //your domain name
+var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+const app = express();
+
+//View Engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('mail');
+});
+
+app.post('/send', (req, res)=> {
+    const data = {
+      from: 'HealthCareAuto <iampavan05@gmail.com>',
+      to: req.body.email,  //req.body.email
+      subject: req.body.subject,
+      text: req.body.message
+    };
+
+    mailgun.messages().send(data, (error, body) => {
+        if(error) {
+            console.log(error);
+            res.render('mail', {msg: "Got Some Error!!!"})
+        }
+      console.log(body);
+
+      res.render('mail', {msg: "Email has been sent"});
+    });    
+});
+
+//listener
+app.listen(3000, () => {
+    console.log('Server Started...');
+})
+
